dac0e19ecf51be834d07f58de5f988e83af47fa0
Hochan Kim <ghckskim@gmail.com>
2024-10-14 18:14:33 +0900

    2024.10.14 리액트 커밋

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+function Header(props){
+  return    <div id="header">
+              <div>
+                <a href="#" onClick={() => {
+                  props.printText();
+                }}>{props.title}</a>
+              </div>
+            </div>
+}
+// Main, Footer 컴포넌트 생성해서 부착
+function Main(props){
+  var list = [];
+
+  props.list.forEach(element => {
+    list.push(<li key={element}> {element} </li>);
+  });
+
+  return  <div id="main">
+            <ul>
+              {list}
+            </ul>
+          </div>
+  // return  <div id="main">
+  //           <ul>
+  //             {props.list.map((subject, index) => (
+  //               <li key={index}>
+  //                 {subject}
+  //               </li>  
+  //             ))}
+  //           </ul>
+  //         </div>
+}
+
+function Footer(props){
+  return  <div id="footer">
+            <a href="#" onClick={() => {
+              props.printText();
+            }}>
+              <p>{props.message}</p>
+            </a>
+          </div>
+}
 
 function App() {
+  var subj1 = [ "자바", "자바스크립트", "오라클" ];
+  var subj2 = [ "html", "css", "리액트", "mysql" ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="자바 재밌다!" printText={() => {
+        console.log("함수 보내기 테스트!!");
+      }}></Header>
+      <Header title="오라클 재밌다!" printText={() => {
+        console.log("함수 보내기 테스트22");
+      }}></Header>
+
+      <Main list={subj1}></Main>
+      <Main list={subj2}></Main>
+      
+      <Footer message="ㅋㅋㅋㅋ" printText={(message) => {
+        alert(message);
+      }}></Footer>
+    </>
   );
 }
 
